742c9cdd3e2577416a32ad65feba8a09bb81b1ce
Ilya <ilya.krapivnoy@gmail.com>
2025-02-14 18:32:59 +0100

    feat: handle form submit

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -35,6 +35,18 @@ class ArticleController extends AbstractController {
             ->add('save', SubmitType::class, array('label' => 'Create', 'attr' => array('class' => 'btn btn-primary mt-3')))
             ->getForm();
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
         return $this->render('articles/new.html.twig', array('form' => $form->createView()));
     }
 
